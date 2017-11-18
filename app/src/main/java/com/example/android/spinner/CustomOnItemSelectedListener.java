04d3f301d93e466badd156e76db6025866241429
tolumutaja <epicbooman3@gmail.com>
2017-11-18 11:02:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/spinner/CustomOnItemSelectedListener.java
@@ -0,0 +1,18 @@
+package com.example.android.spinner;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+}
